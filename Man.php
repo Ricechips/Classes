018c13186cbafd842156db8cf9251e89948e262d
yanzhifanghuoqiang <shiyuhong@hdu.edu.cn>
2019-11-15 13:59:19 +0800

    Update Man.php

--- Man.php
+++ Man.php
@@ -2,8 +2,21 @@
 
 class Man{
   
-  public function __construct(){
-      echo 'Construct a man';
+  public function __construct($age,$name){
+//       echo 'Construct a man';
+    
+      this->_age = $age;
+      this->_name = $name;
   }
+  
+  public function getAge(){
+      return $this->age;
+  }
+  
+  public function getName(){
+      return $this->name;
+  }
+  
+  private $_age,$_name;
   
 }
