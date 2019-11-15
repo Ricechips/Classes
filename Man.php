c45a7dc865ddb83259c7decac243191aae4165bc
yanzhifanghuoqiang <shiyuhong@hdu.edu.cn>
2019-11-15 15:58:26 +0800

    Update Man.php

--- Man.php
+++ Man.php
@@ -7,5 +7,9 @@ class Man extands People{
     public function __construct($age,$name){
       parent::__construct($age,$name,'男')
     }
-
+    
+    public function hi(){
+//         parent::hi();
+        echo 'Man '.$this->getName().' say hi';
+    }
 }
