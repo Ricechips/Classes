f62393afe68b935b4533f3e9502df689fc5e55cc
yanzhifanghuoqiang <shiyuhong@hdu.edu.cn>
2019-11-15 13:55:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
 
-require_once 'Hello1.php';
+// require_once 'Hello1.php';
 
-$h = new Hello();
-$h->sayHello();
+// $h = new Hello();
+// $h->sayHello();
   
+// $d = new Desktop();
+// $d->work();
 
+// require_once 'yzfhq/Hello.php';
+// require_once 'Ricechips/Hello.php';
+
+// $h = new \Ricechips\Hello();
+// $h->sayHello();
+
+// $h = new \yzfhq\Hello();
+// $h->sayHello();
